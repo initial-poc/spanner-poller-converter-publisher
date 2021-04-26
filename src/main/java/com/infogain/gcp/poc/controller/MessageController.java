b0c0cd834ba2cb8d9bf2f48b1bf4492af187a516
gc-user <aarfi.miracle@gmail.com>
2021-04-26 11:01:55 +0530

    controller class added

---
+++ src/main/java/com/infogain/gcp/poc/controller/MessageController.java
@@ -0,0 +1,11 @@
+package com.infogain.gcp.poc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+	
+	
+	//public String processMessge(Me)
+}
