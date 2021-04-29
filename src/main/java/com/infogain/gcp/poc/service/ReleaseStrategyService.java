bdb14eb531f79d2141583a96268cae4ce7064682
initial-poc <chepuri.uk2021@gmail.com>
2021-04-29 11:35:15 +0530

    Issue with release strategy fixed

--- src/main/java/com/infogain/gcp/poc/service/ReleaseStrategyService.java
+++ src/main/java/com/infogain/gcp/poc/service/ReleaseStrategyService.java
@@ -20,40 +20,43 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ReleaseStrategyService {
 
-	private final GroupMessageStoreRepository msgGrpStoreRepository;
+    private final GroupMessageStoreRepository msgGrpStoreRepository;
 
-	@SuppressWarnings("all")
-	public List<PNREntity> release(PNREntity pnrEntity) {
-		log.info("Getting all the messages from the table by pnr id");
-		Optional<List<PNREntity>> pnrEntityList = msgGrpStoreRepository.findByPnrid(pnrEntity.getPnrid());
+    @SuppressWarnings("all")
+    public List<PNREntity> release(PNREntity pnrEntity) {
+        log.info("Getting all the messages from the table by pnr id");
+        Optional<List<PNREntity>> pnrEntityList = msgGrpStoreRepository.findByPnrid(pnrEntity.getPnrid());
 
-		List<PNREntity> pnrList = pnrEntityList.get();
-	//	pnrList.add(pnrEntity);
-		log.info("Messages are {}", pnrList);
-		List<PNREntity> returnList = new ArrayList<PNREntity>();
+        List<PNREntity> pnrList = pnrEntityList.get();
+        //	pnrList.add(pnrEntity);
+        log.info("Messages are {}", pnrList);
+        List<PNREntity> returnList = new ArrayList<PNREntity>();
 
-		Map<Integer, Boolean> seqReleasedStatusMap = pnrList.stream()
-				.collect(Collectors.toMap(PNREntity::getMessageseq, x -> false));
-		Map<Integer, PNREntity> seqPNREntityMap = pnrList.stream()
-				.collect(Collectors.toMap(PNREntity::getMessageseq, x -> x));
+        Map<Integer, Boolean> seqReleasedStatusMap = pnrList.stream()
+                .collect(Collectors.toMap(PNREntity::getMessageseq, x -> x.getStatus().equals(AppConstant.RELEASED) ? true : false));
+        Map<Integer, PNREntity> seqPNREntityMap = pnrList.stream()
+                .collect(Collectors.toMap(PNREntity::getMessageseq, x -> x));
 
-		if (Optional.ofNullable(seqReleasedStatusMap.get(1)).isPresent()) {
-			seqReleasedStatusMap.put(1, true);
-			if (!seqPNREntityMap.get(1).getStatus().equals(AppConstant.RELEASED)) {
-				returnList.add(seqPNREntityMap.get(1));
-			}
-		}
-		log.info("seqReleasedStatusMap {}", seqReleasedStatusMap);
-		log.info("seqPNREntityMap {}", seqPNREntityMap);
-		pnrList.stream().sorted()
-				.filter(x -> seqPNREntityMap.get(x.getMessageseq() - 1) != null
-						? seqReleasedStatusMap.put(x.getMessageseq(), true) || true
-						: false)
-				.filter(y -> !y.getStatus().equals(AppConstant.RELEASED))
-				.filter(u -> (seqReleasedStatusMap.get(u.getMessageseq() - 1)
-						|| seqPNREntityMap.get(u.getMessageseq() - 1).getStatus().equals(AppConstant.RELEASED)))
-				.peek(System.out::println).forEach(z -> returnList.add(z));
-log.info("returning the list {}",returnList);
-		return returnList;
-	}
+        if (Optional.ofNullable(seqReleasedStatusMap.get(1)).isPresent()) {
+            seqReleasedStatusMap.put(1, true);
+            if (!seqPNREntityMap.get(1).getStatus().equals(AppConstant.RELEASED)) {
+                returnList.add(seqPNREntityMap.get(1));
+            }
+        }
+
+        log.info("seqReleasedStatusMap {}", seqReleasedStatusMap);
+        log.info("seqPNREntityMap {}", seqPNREntityMap);
+
+        pnrList.stream().sorted().
+                filter(x -> Optional.ofNullable(seqReleasedStatusMap.get((x.getMessageseq()) - 1)).isPresent()).
+                filter(x -> !seqPNREntityMap.get(x.getMessageseq()).getStatus().equals(AppConstant.RELEASED))
+                .forEach(x -> {
+                    seqReleasedStatusMap.put(x.getMessageseq(), true);
+                    returnList.add(x);
+                });
+
+        log.info("returning the list {}", returnList);
+        return returnList;
+    }
+
 }
